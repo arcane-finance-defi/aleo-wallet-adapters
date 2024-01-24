cc50d7edd40ed895d24e5c8f8964e6827994cdbb
spice2445 <79026145+spice2445@users.noreply.github.com>
2024-01-24 18:02:38 +0200

    version 1.0.7

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,13 @@ export { configureConnection as configureConnectionForPuzzle } from '@puzzlehq/s
 export { LeoWalletAdapter, LeoWalletName } from '@demox-labs/aleo-wallet-adapter-leo';
 export type { LeoWallet, LeoWalletEvents, LeoWindow, LeoWalletAdapterConfig } from '@demox-labs/aleo-wallet-adapter-leo';
 
-export { FoxWalletAdapter, FoxWalletName } from './fox';
-export type { FoxWindow } from './fox';
+export { FoxWalletAdapter, FoxWalletName } from './fox.js';
+export type { FoxWindow } from './fox.js';
 
-export { PuzzleWalletAdapter, PuzzleWalletName } from './puzzle';
-export type { PuzzleWindow, PuzzleWalletAdapterConfig } from './puzzle';
+export { PuzzleWalletAdapter, PuzzleWalletName } from './puzzle.js';
+export type { PuzzleWindow, PuzzleWalletAdapterConfig } from './puzzle.js';
 
-export { SoterWalletAdapter, SoterWalletName } from './soter';
-export type { SoterWindow, SoterWalletAdapterConfig } from './soter';
+export { SoterWalletAdapter, SoterWalletName } from './soter.js';
+export type { SoterWindow, SoterWalletAdapterConfig } from './soter.js';
 
-export { version } from './version';
+export { version } from './version.js';
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+//webpack.config.js
+const path = require('path');
+
+module.exports = {
+  mode: "development",
+  devtool: "inline-source-map",
+  entry: {
+    main: "./index.ts",
+  },
+  output: {
+    clean: true,
+    path: path.resolve(__dirname, '.'),
+    filename: "aleo-adapters-bundle.js",
+    library: {
+      type: "module"
+    }
+  },
+  experiments: {
+    outputModule: true,
+  },
+  resolve: {
+    modules: [
+      path.join(__dirname, './node_modules')
+    ],
+    extensions: [".ts", ".js",],
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.tsx?$/,
+        loader: "ts-loader"
+      }
+    ]
+  }
+};
